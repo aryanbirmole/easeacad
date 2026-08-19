07da12a09a02cb965c851bf318c26dc682c85ceb
aryanbirmole <aryanbirmole@gmail.com>
2026-08-19 16:56:40 +0530

    Wired Navbar and Sidebar into DashboardPage

--- src/pages/DashboardPage.jsx
+++ src/pages/DashboardPage.jsx
@@ -1,11 +1,19 @@
+import Navbar from '../components/common/Navbar';
+import Sidebar from '../components/common/Sidebar';
 import Dashboard from '../components/dashboard/Dashboard';
 
 function DashboardPage() {
   return (
     <div>
-        <h1>EaseAcad</h1>
-        <p>EaseAcad: A Centralized Solution to your Academic Stress!!</p>
-        <Dashboard />
+      <Navbar />
+      <div style={{ display: 'flex' }}>
+        <Sidebar />
+        <div style={{ flex: 1, padding: '24px' }}>
+          <h1>EaseAcad</h1>
+          <p>EaseAcad: A Centralized Solution to your Academic Stress!!</p>
+          <Dashboard />
+        </div>
+      </div>
     </div>
   );
 }
