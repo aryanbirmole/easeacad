7fd2ebb3caefc69dbf775f45066b225a78b1020c
aryanbirmole <aryanbirmole@gmail.com>
2026-08-19 16:53:43 +0530

    Added Navbar component

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,12 @@
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <span className="navbar-title">EaseAcad</span>
+      <div className="navbar-profile">👤</div>
+    </nav>
+  );
+}
+
+export default Navbar;
